3e12d05e4e5ac00160bfbdb20ed5e1e750051095
Tony Vu <tonyvu.dev@gmail.com>
2021-02-06 17:23:54 -0600

    update sql script; delete .iml file

--- sql_scripts/populate_db.sql
+++ sql_scripts/populate_db.sql
@@ -7,6 +7,9 @@ INSERT INTO metro_bank.employee (
 	emp_phone
 ) VALUES 
 ('Bobby', 'Dolphin', 'Manager', 'b.dolphin@metrobank.com', '651-555-9987'),
-('Britney', 'Orlando', 'Supervisor', 'b.Orlando@metrobank.com', '612-555-1234'),
+('Tung', 'Vu', 'Student', 'Tung.Vu@metrostate.com', '612-555-1234'),
+('Anthony', 'Nguyen', 'Student', 'Anthony.Nguyen@metrostate.com', '612-555-1233'),
+('Safina', 'Nganga', 'Student', 'Safina.Nganga@metrostate.com', '612-555-1334'),
+('Janaya', 'Thomas', 'Student', 'Janaya.Thomas@metrostate.com', '612-555-2334'),
 ('Leona', 'Dunne', 'Teller', 'l.Dunne@metrobank.com', '952-555-1177'),
 ('Walter', 'Smith', 'Teller', 'w.Smith@metrobank.com', '612-555-1234');
