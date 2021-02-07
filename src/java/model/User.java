f253a249e4260ef8c723549660aad6895b210f9e
Tony Vu <tonyvu.dev@gmail.com>
2021-02-06 18:22:59 -0600

    add User model

---
+++ src/java/model/User.java
@@ -0,0 +1,77 @@
+package model;
+
+public class User {
+    int id;
+    String fname;
+    String lname;
+    String phone;
+    String role;
+    String email;
+    String password;
+
+    public User(int id, String fname, String lname, String phone, String role, String email, String password) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
