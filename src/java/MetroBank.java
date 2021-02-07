dea3dfa44ea90d76a9b59e259b2bb2e9ecd49ce3
Tony Vu <tonyvu.dev@gmail.com>
2021-02-06 18:23:44 -0600

    implement select query to grab user on login

--- src/java/MetroBank.java
+++ src/java/MetroBank.java
@@ -1,4 +1,5 @@
-import java.sql.ResultSet;
+import gui.MySQLConnect;
+
 import java.sql.SQLException;
 import java.util.*;
 
--- src/java/gui/MySQLConnect.java
+++ src/java/gui/MySQLConnect.java
@@ -1,3 +1,5 @@
+package gui;
+
 import java.sql.*;
 import java.util.*;
 
